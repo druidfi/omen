5d4b49f9437d361dae0683c1870889c04828f7f7
Marko Korhonen <marko.korhonen@druid.fi>
2019-11-01 13:09:50 +0200

    Wodby mapping and tests

--- src/Env/Wodby.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-/**
- * @see https://wodby.com/docs/infrastructure/env-vars/
- */
-
---
+++ src/EnvMapping/Wodby.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @see https://wodby.com/docs/infrastructure/env-vars/
+ */
+
+return [
+  'APP_ENV' => 'WODBY_INSTANCE_TYPE',
+  'HOSTNAME' => 'WODBY_HOST_PRIMARY',
+  'DRUPAL_DB_NAME' => 'DB_NAME',
+  'DRUPAL_DB_USER' => 'DB_USER',
+  'DRUPAL_DB_PASS' => 'DB_PASSWORD',
+  'DRUPAL_DB_HOST' => 'DB_HOST',
+  'DRUPAL_DB_PORT' => 'MARIADB_SERVICE_PORT',
+];
