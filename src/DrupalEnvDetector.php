6d7ea635e2d482fe1f791273bf88ae1f8c1ebc41
Marko Korhonen <marko.korhonen@druid.fi>
2021-12-02 20:28:11 +0200

    Typo

--- src/DrupalEnvDetector.php
+++ src/DrupalEnvDetector.php
@@ -7,7 +7,7 @@ class DrupalEnvDetector
   public function __construct(string $settings_dir)
   {
     echo "<h1>BC BREAK!</h1>";
-    echo "<p>Update your call in settings.php in ". $$settings_dir ."</p>";
+    echo "<p>Update your call in settings.php in ". $settings_dir ."</p>";
     echo "<p><strong>Old line:</strong>";
     echo "<p><code>extract((new Druidfi\Omen\DrupalEnvDetector(__DIR__))->getConfiguration());</code>";
     echo "<p><strong>New line:</strong>";
