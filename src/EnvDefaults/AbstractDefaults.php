11bee575c55c4d4ddc2de117074e53cafbb442eb
Marko Korhonen <marko.korhonen@druid.fi>
2021-12-02 13:27:29 +0200

    Simplyfy Defaults classes

--- src/EnvDefaults/AbstractDefaults.php
+++ src/EnvDefaults/AbstractDefaults.php
@@ -1,27 +1,16 @@
-<?php declare(strict_types=1);
+<?php
 
 namespace Druidfi\Omen\EnvDefaults;
 
 abstract class AbstractDefaults {
+
   protected array $config = [];
   protected array $settings = [];
 
   public function getDefaults() : array {
-    $contrib_module_defaults = $this->getContribModuleDefaults();
-
-    $config = array_merge($this->config, $contrib_module_defaults['config']);
-    $settings = array_merge($this->settings, $contrib_module_defaults['settings']);
-
     return [
-      'config' => $config,
-      'settings' => $settings,
-    ];
-  }
-
-  protected function getContribModuleDefaults() : array {
-    return [
-      'config' => [],
-      'settings' => [],
+      'config' => $this->config,
+      'settings' => $this->settings,
     ];
   }
 }
--- src/EnvDefaults/DevDefaults.php
+++ src/EnvDefaults/DevDefaults.php
@@ -1,36 +1,33 @@
-<?php declare(strict_types=1);
+<?php
 
 namespace Druidfi\Omen\EnvDefaults;
 
 class DevDefaults extends AbstractDefaults {
-  public function __construct()
-  {
-    // Skip file system permissions hardening.
-    $settings['skip_permissions_hardening'] = TRUE;
 
-    // Show all error messages on the site.
-    $config['system.logging']['error_level'] = 'all';
+  protected array $config = [
+    'stage_file_proxy.settings' => [
+      'origin' => TRUE,
+    ],
+    'system.logging' => [
+      'error_level' => 'all'
+    ],
+    'system.performance' => [
+      'cache' => [
+        'page' => [
+          'max_age' => 0,
+        ]
+      ],
+      'css' => [
+        'preprocess' => 0,
+      ],
+      'js' => [
+        'preprocess' => 0,
+      ],
+    ],
+  ];
 
-    // Expiration of cached pages to 0.
-    $config['system.performance']['cache']['page']['max_age'] = 0;
-
-    // Aggregate CSS files off.
-    $config['system.performance']['css']['preprocess'] = 0;
-
-    // Aggregate JavaScript files off.
-    $config['system.performance']['js']['preprocess'] = 0;
-
-    $this->config = $config;
-    $this->settings = $settings;
-  }
-
-  protected function getContribModuleDefaults() : array {
-    // Simple Environment Indicator.
-    $settings['simple_environment_indicator'] = 'Black Development';
-
-    return [
-      'config' => [],
-      'settings' => $settings,
-    ];
-  }
+  protected array $settings = [
+    'simple_environment_indicator' => 'Black Development', // Simple Environment Indicator.
+    'skip_permissions_hardening' => FALSE,
+  ];
 }
